98f0aac98b252ff4498bfe079ceac5ac94ad8b3e
Andreas Pedersen <31003640+AndreasP-92@users.noreply.github.com>
2022-09-08 19:21:25 +0200

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,18 @@
-import * as React from 'react';
-import { Outlet, Link } from 'react-router-dom';
-import { Carousel } from 'react-bootstrap';
+import React from 'react';
+import { Link } from 'react-router-dom';
+import SliderComponent from './components/SliderComponent';
+import Button from './components/Button';
+
+const alertTest = ()=>{
+  alert("test")
+}
 
 export default function App() {
   return (
     <div>
       <h1>Bookkeeper</h1>
-      <Carousel/>
+      <SliderComponent/>
+      <Button onClick={alertTest} text="Click me"/>
       <div style={{border: '1px solid black'}}>
         <p>Dette er et event
         <Link to="/eventdetails">Dette er et link til et event</Link>
--- src/index.js
+++ src/index.js
@@ -1,20 +1,22 @@
 import  React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import App from './App';
 import EventDetail from './pages/EventDetails'
 import EventDetail2 from './pages/EventDetails2'
 import './index.css';
 
-const root = ReactDOM.createRoot( document.getElementById('root'));
-root.render(
-  <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<App />}>
-      </Route>
-      <Route path="eventdetails" element={<EventDetail />} />
-      <Route path="eventdetails2" element={<EventDetail2 />} />
-    </Routes>
-  </BrowserRouter>
-  
+const rootElement = document.getElementById('root');
+ReactDOM.render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />}>
+        </Route>
+        <Route path="eventdetails" element={<EventDetail />} />
+        <Route path="eventdetails2" element={<EventDetail2 />} />
+      </Routes>
+    </BrowserRouter>
+  </React.StrictMode>,
+  rootElement
 );
