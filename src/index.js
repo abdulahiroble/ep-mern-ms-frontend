7451d5da4a74938a88bdccfc5485c33728b5001a
Jeffrey <jeffoech@gmail.com>
2022-11-27 16:21:03 +0100

    Login has prblems, with implementing it

--- src/index.js
+++ src/index.js
@@ -1,22 +1,39 @@
 import  React from 'react';
-import ReactDOM from 'react-dom';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import App from './App';
+import './index.css';
+import ReactDOM from 'react-dom';
+import { BrowserRouter, Routes, Route, Redirect, Switch} from 'react-router-dom';
+import RouteGuard from './components/RouteGuard';
+
+//history
+import { history } from './helpers/history';
+
+//pages
 import EventDetail from './pages/EventDetails'
 import EventDetail2 from './pages/EventDetails2'
 import Login from './pages/Login';
-import './index.css';
+
 
 const rootElement = document.getElementById('root');
 ReactDOM.render(
     <BrowserRouter>
-      <Routes>
+      <Routes history={history}>
         <Route path="/" element={<App />}>
+          <Switch>
+            <RouteGuard
+              exact
+              path='/'
+              component={EventDetail2}
+            />
+            <Route path='login' element={<Login/>}/>
+          </Switch>
         </Route>
         <Route path="eventdetails/:id" element={<EventDetail />} />
         <Route path="eventdetails2" element={<EventDetail2 />} />
-        <Route path='login' element={<Login/>}/>
+        <Redirect to ='/'/>
       </Routes>
     </BrowserRouter>,
     rootElement
 );
+
+export default Routes;
