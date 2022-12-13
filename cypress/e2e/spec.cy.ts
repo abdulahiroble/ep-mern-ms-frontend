6ea19c2151f2a53fd4b5fb5fe05f4a843d472e88
abdulahi roble <abdulahi_2610@hotmail.com>
2022-12-13 21:09:37 +0100

    Initial end to end test cases

--- cypress/e2e/spec.cy.ts
+++ cypress/e2e/spec.cy.ts
@@ -1,18 +1,27 @@
-describe("empty spec", () => {
+describe("Sign in user and search event by zipcode", () => {
   it("passes", () => {
 
 
-    // login
+    // Visit the app root
     cy.visit("http://localhost:3000");
-    // navigate to login page
-    // wait 3 seconds
     cy.wait(2000);
+    // navigate to login page
     cy.findByRole("link", {name: /login/i}).click();
+
+    // fill in the form
     cy.findByRole("textbox", {name: /email/i}).type("sejenu@gmail.com");
     cy.findByLabelText(/password/i).type("naruto123");
+
+    // submit the form
     cy.findByRole('button', {name: /log in/i}).click();
+
+    // wait for the page to load
     cy.wait(5000);
+
+    // search for event by zipcode
     cy.findByRole('textbox').type("2610")
+
+    // click on search button
     cy.findByRole('button', {name: /search/i}).click();
 
   });
