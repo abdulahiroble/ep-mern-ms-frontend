f25924200c309794eb36959af5817dc60e4b40e5
abdulahi roble <abdulahi_2610@hotmail.com>
2022-12-13 20:13:07 +0100

    Create first scenario

--- cypress/e2e/spec.cy.ts
+++ cypress/e2e/spec.cy.ts
@@ -1,5 +1,19 @@
 describe("empty spec", () => {
   it("passes", () => {
-    cy.visit("https://example.cypress.io");
+
+
+    // login
+    cy.visit("http://localhost:3000");
+    // navigate to login page
+    // wait 3 seconds
+    cy.wait(2000);
+    cy.findByRole("link", {name: /login/i}).click();
+    cy.findByRole("textbox", {name: /email/i}).type("sejenu@gmail.com");
+    cy.findByLabelText(/password/i).type("naruto123");
+    cy.findByRole('button', {name: /log in/i}).click();
+    cy.wait(5000);
+    cy.findByRole('textbox').type("2610")
+    cy.findByRole('button', {name: /search/i}).click();
+
   });
 });
