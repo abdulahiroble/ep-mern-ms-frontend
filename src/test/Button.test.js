d4c148240bfc1597d1741c0022cb5f014fccf0be
abdulahi roble <abdulahi_2610@hotmail.com>
2022-12-14 19:46:06 +0100

    Grab registration component and test

--- src/test/Button.test.js
+++ src/test/Button.test.js
@@ -1,9 +1,23 @@
 import React from 'react';
-import {render, fireEvent} from '@testing-library/react';
+import {render, fireEvent, screen} from '@testing-library/react';
 import Button from '../components/Button';
-import {screen, configure} from '@testing-library/react'
+// import {screen, configure} from '@testing-library/react'
 import '@testing-library/jest-dom'
+import RegistrationForm from '../components/RegistrationForm';
 
+Object.defineProperty(window, 'matchMedia', {
+    writable: true,
+    value: jest.fn().mockImplementation(query => ({
+        matches: false,
+        media: query,
+        onchange: null,
+        addListener: jest.fn(), // Deprecated
+        removeListener: jest.fn(), // Deprecated
+        addEventListener: jest.fn(),
+        removeEventListener: jest.fn(),
+        dispatchEvent: jest.fn(),
+    })),
+});
 
 describe('Button', () => {
     it('should render a button with the provided text', () => {
@@ -19,3 +33,15 @@ describe('Button', () => {
         expect(onClick).toHaveBeenCalled();
     });
 });
+
+
+describe('First Name', () => {
+    // let cases = [[frank, true], [jasmine, false]]
+
+    it('should have more than 2 characters', () => {
+        render(<RegistrationForm />);
+        const firstName = screen.getByLabelText('First name');
+        fireEvent.change(firstName, {target: {value: 'abdulahi'}});
+        expect(firstName.value).toBe('abdulahi');
+    });
+})
