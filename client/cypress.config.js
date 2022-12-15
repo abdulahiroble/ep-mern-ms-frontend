e48a1192204b938f5ece16a93463ffee4fc25e24
abdulahi roble <abdulahi_2610@hotmail.com>
2022-12-15 17:19:05 +0100

    Pass url and change script

--- client/cypress.config.js
+++ client/cypress.config.js
@@ -6,13 +6,27 @@ module.exports = defineConfig({
   reporter: 'cypress-mochawesome-reporter',
 
   e2e: {
-
+    baseUrl: process.env.PUBLIC_URL,
     setupNodeEvents(on, config) {
-
       require('cypress-mochawesome-reporter/plugin')(on);
-
     },
 
   },
 
 });
+
+// const {defineConfig} = require('cypress')
+
+// module.exports = defineConfig({
+//   reporter: 'cypress-mochawesome-reporter',
+  
+//   e2e: {
+//     baseUrl: 'http://localhost:3000',
+//     reporter: 'reporters/custom.js',
+//     setupNodeEvents(on, config) {
+
+//       require('cypress-mochawesome-reporter/plugin')(on);
+
+//     },
+//   }
+// })
