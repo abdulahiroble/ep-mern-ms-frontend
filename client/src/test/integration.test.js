7ab31f693f0a04ccf659efea61f22b9c2844f356
AndreasB2B <99027014+AndreasB2B@users.noreply.github.com>
2022-12-16 16:10:27 +0100

    update pagination

--- client/src/test/integration.test.js
+++ client/src/test/integration.test.js
@@ -4,7 +4,6 @@ describe('Find user', () => {
     it('Testing if it is possible to find a user', async () => {
 
         const result = await ApiContext.LoadUserCollection.getUserProfile('636f93b4c9d50cff9ed21659')
-        console.log(result)
         expect(result.status).toBe(200);
     });
 })
