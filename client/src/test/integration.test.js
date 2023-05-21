a9e0bf788acf1515c2f65fe4fc785d6098f5613b
Andreas Pedersen <31003640+AndreasP-92@users.noreply.github.com>
2023-05-21 04:48:56 +0200

    dd

--- client/src/test/integration.test.js
+++ client/src/test/integration.test.js
@@ -1,14 +1,3 @@
-// import ApiContext from '../services/ApiContext'
-
-// describe('Find user', () => {
-//     it('Testing if it is possible to find a user', async () => {
-
-//         const result = await ApiContext.LoadUserCollection.getUserProfile('636f93b4c9d50cff9ed21659')
-//         expect(result.status).toBe(200);
-//     });
-// })
-
-
 describe('tmp test', () => {
     it('Testing if it is possible to find a user', async () => {
 
